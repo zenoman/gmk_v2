33c73cfed8db1a6dc4b3944e607b5e788ea382ab
deva satrio <satriosuklun@gmail.com>
2019-06-18 20:19:21 +0700

    fixing gmk new + db baru
    nama database "gmk_new"

---
+++ gmk_new.sql
@@ -0,0 +1,444 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.1.30-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win32
+-- HeidiSQL Version:             9.4.0.5174
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Dumping database structure for gmk_new
+DROP DATABASE IF EXISTS `gmk_new`;
+CREATE DATABASE IF NOT EXISTS `gmk_new` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `gmk_new`;
+
+-- Dumping structure for table gmk_new.admins
+DROP TABLE IF EXISTS `admins`;
+CREATE TABLE IF NOT EXISTS `admins` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(80) DEFAULT NULL,
+  `password` text,
+  `nama` varchar(100) DEFAULT NULL,
+  `telp` varchar(25) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `level` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
+
+-- Dumping data for table gmk_new.admins: ~8 rows (approximately)
+DELETE FROM `admins`;
+/*!40000 ALTER TABLE `admins` DISABLE KEYS */;
+INSERT INTO `admins` (`id`, `username`, `password`, `nama`, `telp`, `email`, `level`) VALUES
+	(10, 'devasatrio', '$2y$10$7d6KjJtH/GfK.45S1R9JAefDZgcCF3CW0DiW68a4FoTjiyGM/yUBS', 'deva satrio damara', '023934820948', 'satriosuklun@gmail.com', 'programer'),
+	(11, 'diansetiawan', '$2y$10$CpKmLECL3v8nVnL37Tb20ugH8QMjugXapUyuTpEhuXxziVvqddiWm', 'dian ade setiawan', '085623497800', 'dianade@gmail.com', 'programer'),
+	(12, 'adminsatu', '$2y$10$./4I24ToWf90yexH24nNr.C.hUtaTewshvVENi9d8bvHYxxNY/rsq', 'admin ke satu', '+6285816900612', 'admin1@gmail.com', 'admin'),
+	(13, 'abiihsan', '$2y$10$HtZx6PUklxwaaFntiHqV5.4BZiXNEXdF1eA/J.ce701M5Thi7RLki', 'abi ihsan fadli', '2093482903480', 'abi@gmail.com', 'programer'),
+	(14, 'taufiqperdana', '$2y$10$734GwOfWOeNB6gdtZqR7ZutUB8FzjPaupBypdRsFjypj3RFnQMKFa', 'M. taufiq perdana', '023984290380', 'taufiq@gmail.com', 'programer'),
+	(15, 'admindua', '$2y$10$0/s.qgxDCDscTqNc42wdseaI.CaHYuv4z2gqYkG7xMLRxPTO.KVt6', 'admin ke dua', '20348239048902', 'admin@gmail.com', 'admin'),
+	(16, 'ownerstteam', '$2y$10$vJTnhlwQ9r2KbPeS4zp0Y.BBBg0Ovqj6vdqxT5m7DCvNqarGSFWfq', 'owner tastore', '085856044060', 'owner@gmail.com', 'super_admin'),
+	(17, 'admin2lagi', '$2y$10$YtYyE6TpxSHedx0d6v4Z8edQdptFEy/kyNXyK9JvNpqXorODx9xPK', 'admin 2', '085765427837', 'admin2lagi@gmail.com', 'admin');
+/*!40000 ALTER TABLE `admins` ENABLE KEYS */;
+
+-- Dumping structure for table gmk_new.detail_cancel
+DROP TABLE IF EXISTS `detail_cancel`;
+CREATE TABLE IF NOT EXISTS `detail_cancel` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `idwarna` int(11) DEFAULT NULL,
+  `iduser` int(11) DEFAULT NULL,
+  `kode` text,
+  `tgl` date DEFAULT NULL,
+  `jumlah` int(11) DEFAULT NULL,
+  `harga` int(11) DEFAULT NULL,
+  `barang` varchar(100) DEFAULT NULL,
+  `total` int(11) DEFAULT NULL,
+  `diskon` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table gmk_new.detail_cancel: ~0 rows (approximately)
+DELETE FROM `detail_cancel`;
+/*!40000 ALTER TABLE `detail_cancel` DISABLE KEYS */;
+/*!40000 ALTER TABLE `detail_cancel` ENABLE KEYS */;
+
+-- Dumping structure for table gmk_new.gambar
+DROP TABLE IF EXISTS `gambar`;
+CREATE TABLE IF NOT EXISTS `gambar` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `kode_barang` varchar(30) DEFAULT NULL,
+  `nama` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=240 DEFAULT CHARSET=latin1;
+
+-- Dumping data for table gmk_new.gambar: ~0 rows (approximately)
+DELETE FROM `gambar`;
+/*!40000 ALTER TABLE `gambar` DISABLE KEYS */;
+/*!40000 ALTER TABLE `gambar` ENABLE KEYS */;
+
+-- Dumping structure for table gmk_new.keranjang_cancel
+DROP TABLE IF EXISTS `keranjang_cancel`;
+CREATE TABLE IF NOT EXISTS `keranjang_cancel` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tgl` date DEFAULT NULL,
+  `idbarang` int(11) NOT NULL DEFAULT '0',
+  `jumlah` int(11) NOT NULL DEFAULT '0',
+  `harga` int(11) DEFAULT NULL,
+  `total` int(11) DEFAULT NULL,
+  `admin` int(11) DEFAULT NULL,
+  `status` enum('Y','N') DEFAULT 'N',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table gmk_new.keranjang_cancel: ~0 rows (approximately)
+DELETE FROM `keranjang_cancel`;
+/*!40000 ALTER TABLE `keranjang_cancel` DISABLE KEYS */;
+/*!40000 ALTER TABLE `keranjang_cancel` ENABLE KEYS */;
+
+-- Dumping structure for table gmk_new.log_cancel
+DROP TABLE IF EXISTS `log_cancel`;
+CREATE TABLE IF NOT EXISTS `log_cancel` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `faktur` varchar(300) DEFAULT NULL,
+  `total_akhir` int(11) DEFAULT NULL,
+  `tgl` date DEFAULT NULL,
+  `bulan` int(5) DEFAULT NULL,
+  `status` enum('dicancel','ditolak') DEFAULT NULL,
+  `id_user` int(11) DEFAULT NULL,
+  `id_admin` int(11) DEFAULT NULL,
+  `keterangan` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table gmk_new.log_cancel: ~0 rows (approximately)
+DELETE FROM `log_cancel`;
+/*!40000 ALTER TABLE `log_cancel` DISABLE KEYS */;
+/*!40000 ALTER TABLE `log_cancel` ENABLE KEYS */;
+
+-- Dumping structure for table gmk_new.migrations
+DROP TABLE IF EXISTS `migrations`;
+CREATE TABLE IF NOT EXISTS `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Dumping data for table gmk_new.migrations: ~0 rows (approximately)
+DELETE FROM `migrations`;
+/*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
+/*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
+
+-- Dumping structure for table gmk_new.omset
+DROP TABLE IF EXISTS `omset`;
+CREATE TABLE IF NOT EXISTS `omset` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pemasukan_online` int(11) DEFAULT '0',
+  `pemasukan_offline` int(11) DEFAULT '0',
+  `pemasukan_lain` int(11) DEFAULT '0',
+  `pengeluaran` int(11) DEFAULT '0',
+  `omset` int(11) DEFAULT '0',
+  `bulan` int(11) DEFAULT '0',
+  `tahun` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table gmk_new.omset: ~0 rows (approximately)
+DELETE FROM `omset`;
+/*!40000 ALTER TABLE `omset` DISABLE KEYS */;
+/*!40000 ALTER TABLE `omset` ENABLE KEYS */;
+
+-- Dumping structure for table gmk_new.settings
+DROP TABLE IF EXISTS `settings`;
+CREATE TABLE IF NOT EXISTS `settings` (
+  `idsettings` int(11) NOT NULL AUTO_INCREMENT,
+  `webName` varchar(100) DEFAULT NULL,
+  `kontak1` varchar(45) DEFAULT NULL,
+  `kontak2` varchar(45) DEFAULT NULL,
+  `kontak3` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `ico` varchar(45) DEFAULT NULL,
+  `meta` text,
+  `logo` varchar(50) DEFAULT NULL,
+  `keterangan` text,
+  `alamat` text,
+  `nama_toko` int(11) DEFAULT NULL,
+  `max_tgl` int(5) DEFAULT NULL,
+  `peraturan` text,
+  `bulansistem` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idsettings`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+-- Dumping data for table gmk_new.settings: ~0 rows (approximately)
+DELETE FROM `settings`;
+/*!40000 ALTER TABLE `settings` DISABLE KEYS */;
+INSERT INTO `settings` (`idsettings`, `webName`, `kontak1`, `kontak2`, `kontak3`, `email`, `ico`, `meta`, `logo`, `keterangan`, `alamat`, `nama_toko`, `max_tgl`, `peraturan`, `bulansistem`) VALUES
+	(1, 'STORE TULUNGAGUNG', '+6285816900612', '+6281553860046', '+6285856044060', 'bayukhoirul1@gmail.com', '1547722245-dvinafavicon.png', 'Fashion murah meriah berkualitas', '1543717647-logo-dvina.png', 'store tulungagung adalah toko ecer / grosir yang telah terbukti memiliki harga dan kwalitas terbaik\r\nopen reseller , dropshiper, agen. store tulungagung selalu berusaha memberikan pelayanan terbaik kepada pembeli. dan banyak potongan harga untuk pembelian lebih dari 3pcs.', 'Dsn.Jatirejo RT01/RW02 Ds.Tenggur Kec.Rejotangan Kab.Tulungagung', NULL, 7, '<p>1. pastikan telah menjadi member StoreTulungagung</p><p>2. pastikan melakukan pemesanan / keep barang sampai masuk pada W.A admin</p><p>3. jangan lupa bayar setelah beli produk</p><p>4. setiap barang yang telah di masukan keranjang akan hilang secara otomatis apabila tidak di beli dalam jangka waktu 7hari</p><p>5. Happy Shopping gengs</p>', 6);
+/*!40000 ALTER TABLE `settings` ENABLE KEYS */;
+
+-- Dumping structure for table gmk_new.sliders
+DROP TABLE IF EXISTS `sliders`;
+CREATE TABLE IF NOT EXISTS `sliders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `judul` varchar(100) DEFAULT NULL,
+  `foto` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+-- Dumping data for table gmk_new.sliders: ~3 rows (approximately)
+DELETE FROM `sliders`;
+/*!40000 ALTER TABLE `sliders` DISABLE KEYS */;
+INSERT INTO `sliders` (`id`, `judul`, `foto`) VALUES
+	(1, 'ini slide 1', '1548724725-tabanner2.jpg'),
+	(2, 'ini slide 2', '1548724768-tabanner3.jpg'),
+	(3, 'ini slide 3', '1548724999-tabanner1.gif');
+/*!40000 ALTER TABLE `sliders` ENABLE KEYS */;
+
+-- Dumping structure for table gmk_new.tb_bank
+DROP TABLE IF EXISTS `tb_bank`;
+CREATE TABLE IF NOT EXISTS `tb_bank` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama_bank` varchar(40) DEFAULT NULL,
+  `rekening` varchar(40) DEFAULT NULL,
+  `atasnama` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+-- Dumping data for table gmk_new.tb_bank: ~4 rows (approximately)
+DELETE FROM `tb_bank`;
+/*!40000 ALTER TABLE `tb_bank` DISABLE KEYS */;
+INSERT INTO `tb_bank` (`id`, `nama_bank`, `rekening`, `atasnama`) VALUES
+	(1, 'bayar ditoko\r\n', '-', '-'),
+	(2, 'mandiri Syariah', '09737897890', 'storeta'),
+	(3, 'BRI', '902890890', 'storeta'),
+	(4, 'bank jatim', '09890890890', 'storeta');
+/*!40000 ALTER TABLE `tb_bank` ENABLE KEYS */;
+
+-- Dumping structure for table gmk_new.tb_barangs
+DROP TABLE IF EXISTS `tb_barangs`;
+CREATE TABLE IF NOT EXISTS `tb_barangs` (
+  `idbarang` int(11) NOT NULL AUTO_INCREMENT,
+  `kode` varchar(100) DEFAULT NULL,
+  `kode_v` varchar(100) DEFAULT NULL,
+  `stok` int(11) DEFAULT NULL,
+  `warna` varchar(45) DEFAULT NULL,
+  `barang_jenis` varchar(70) DEFAULT NULL,
+  PRIMARY KEY (`idbarang`)
+) ENGINE=InnoDB AUTO_INCREMENT=301 DEFAULT CHARSET=latin1;
+
+-- Dumping data for table gmk_new.tb_barangs: ~0 rows (approximately)
+DELETE FROM `tb_barangs`;
+/*!40000 ALTER TABLE `tb_barangs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_barangs` ENABLE KEYS */;
+
+-- Dumping structure for table gmk_new.tb_details
+DROP TABLE IF EXISTS `tb_details`;
+CREATE TABLE IF NOT EXISTS `tb_details` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `idwarna` int(11) DEFAULT NULL,
+  `iduser` int(11) DEFAULT NULL,
+  `kode_v` varchar(50) DEFAULT NULL,
+  `faktur` varchar(100) DEFAULT NULL,
+  `tgl` date DEFAULT NULL,
+  `tgl_kadaluarsa` date DEFAULT NULL,
+  `kode_barang` varchar(100) DEFAULT NULL,
+  `barang` varchar(100) DEFAULT NULL,
+  `harga` int(11) DEFAULT NULL,
+  `jumlah` int(11) DEFAULT NULL,
+  `total_a` int(11) DEFAULT NULL,
+  `diskon` int(11) DEFAULT NULL,
+  `total` int(11) DEFAULT NULL,
+  `sb` enum('1','0') NOT NULL DEFAULT '0',
+  `admin` varchar(100) DEFAULT NULL,
+  `metode` enum('langsung','pesan') DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table gmk_new.tb_details: ~0 rows (approximately)
+DELETE FROM `tb_details`;
+/*!40000 ALTER TABLE `tb_details` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_details` ENABLE KEYS */;
+
+-- Dumping structure for table gmk_new.tb_kategoris
+DROP TABLE IF EXISTS `tb_kategoris`;
+CREATE TABLE IF NOT EXISTS `tb_kategoris` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `kategori` varchar(100) DEFAULT NULL,
+  `gambar` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+-- Dumping data for table gmk_new.tb_kategoris: ~0 rows (approximately)
+DELETE FROM `tb_kategoris`;
+/*!40000 ALTER TABLE `tb_kategoris` DISABLE KEYS */;
+INSERT INTO `tb_kategoris` (`id`, `kategori`, `gambar`) VALUES
+	(1, 'kemeja', '1560863420-da.jpg');
+/*!40000 ALTER TABLE `tb_kategoris` ENABLE KEYS */;
+
+-- Dumping structure for table gmk_new.tb_kodes
+DROP TABLE IF EXISTS `tb_kodes`;
+CREATE TABLE IF NOT EXISTS `tb_kodes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_kategori` int(11) DEFAULT NULL,
+  `kode_barang` varchar(100) DEFAULT NULL,
+  `barang` varchar(150) DEFAULT NULL,
+  `harga_beli` int(11) DEFAULT NULL,
+  `harga_barang` int(11) DEFAULT NULL,
+  `harga_reseller` int(11) DEFAULT NULL,
+  `deskripsi` mediumtext,
+  `diskon` int(11) DEFAULT '0',
+  `tampil` enum('Y','N') DEFAULT 'Y',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=179 DEFAULT CHARSET=latin1;
+
+-- Dumping data for table gmk_new.tb_kodes: ~0 rows (approximately)
+DELETE FROM `tb_kodes`;
+/*!40000 ALTER TABLE `tb_kodes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_kodes` ENABLE KEYS */;
+
+-- Dumping structure for table gmk_new.tb_stokawals
+DROP TABLE IF EXISTS `tb_stokawals`;
+CREATE TABLE IF NOT EXISTS `tb_stokawals` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `idbarang` int(11) DEFAULT NULL,
+  `idwarna` int(11) DEFAULT NULL,
+  `kode_barang` varchar(100) DEFAULT NULL,
+  `barang` varchar(100) DEFAULT NULL,
+  `jumlah` int(11) DEFAULT NULL,
+  `tgl` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table gmk_new.tb_stokawals: ~0 rows (approximately)
+DELETE FROM `tb_stokawals`;
+/*!40000 ALTER TABLE `tb_stokawals` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_stokawals` ENABLE KEYS */;
+
+-- Dumping structure for table gmk_new.tb_tambahstoks
+DROP TABLE IF EXISTS `tb_tambahstoks`;
+CREATE TABLE IF NOT EXISTS `tb_tambahstoks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `idwarna` int(11) DEFAULT NULL,
+  `idadmin` int(11) DEFAULT NULL,
+  `kode_barang` varchar(150) DEFAULT NULL,
+  `jumlah` int(11) DEFAULT NULL,
+  `total` int(11) DEFAULT NULL,
+  `tgl` date DEFAULT NULL,
+  `keterangan` varchar(100) DEFAULT NULL,
+  `aksi` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table gmk_new.tb_tambahstoks: ~0 rows (approximately)
+DELETE FROM `tb_tambahstoks`;
+/*!40000 ALTER TABLE `tb_tambahstoks` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_tambahstoks` ENABLE KEYS */;
+
+-- Dumping structure for table gmk_new.tb_transaksis
+DROP TABLE IF EXISTS `tb_transaksis`;
+CREATE TABLE IF NOT EXISTS `tb_transaksis` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `iduser` int(11) DEFAULT NULL,
+  `faktur` varchar(300) DEFAULT NULL,
+  `tgl` date DEFAULT NULL,
+  `total` int(11) DEFAULT NULL,
+  `status` enum('terkirim','dibaca','diterima','ditolak','sukses','batal') DEFAULT NULL,
+  `alamat_tujuan` text,
+  `admin` varchar(100) DEFAULT NULL,
+  `ongkir` int(11) DEFAULT '0',
+  `potongan` int(11) DEFAULT '0',
+  `total_akhir` int(11) DEFAULT NULL,
+  `pembayaran` varchar(50) DEFAULT NULL,
+  `keterangan` text,
+  `metode` enum('pesan','langsung') DEFAULT 'pesan',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table gmk_new.tb_transaksis: ~0 rows (approximately)
+DELETE FROM `tb_transaksis`;
+/*!40000 ALTER TABLE `tb_transaksis` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_transaksis` ENABLE KEYS */;
+
+-- Dumping structure for table gmk_new.tb_users
+DROP TABLE IF EXISTS `tb_users`;
+CREATE TABLE IF NOT EXISTS `tb_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) DEFAULT NULL,
+  `password` text,
+  `email` varchar(100) DEFAULT NULL,
+  `telp` varchar(45) DEFAULT NULL,
+  `nama` varchar(100) DEFAULT NULL,
+  `alamat` text,
+  `kota` varchar(100) DEFAULT NULL,
+  `provinsi` varchar(100) DEFAULT NULL,
+  `kodepos` varchar(45) DEFAULT NULL,
+  `level` enum('biasa','reseller') DEFAULT 'biasa',
+  `ktp_gmb` varchar(100) DEFAULT NULL,
+  `cancel` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+-- Dumping data for table gmk_new.tb_users: ~3 rows (approximately)
+DELETE FROM `tb_users`;
+/*!40000 ALTER TABLE `tb_users` DISABLE KEYS */;
+INSERT INTO `tb_users` (`id`, `username`, `password`, `email`, `telp`, `nama`, `alamat`, `kota`, `provinsi`, `kodepos`, `level`, `ktp_gmb`, `cancel`) VALUES
+	(1, 'jianfitri', '$2y$10$lzwpAHH/1XazX.datgWJDO9f51y2oBWbQlTviAjb3/.o2fTjTsFQi', 'jian@gmail.com', '0856045567140', 'jian fitri aprilia', 'gurah kediri', 'kediri', 'jawa timur', '0934', 'reseller', '1554002975-kaos1.jpeg', 0),
+	(2, 'rinookta', '$2y$10$YfN61LOcjeLTrz.EOrNJCuj2LCOkFJa8wuUhGsmDqUE6gdXLaV32.', 'rino@gmail.com', '2093842903', 'rino oktavian', 'sjadklfjskladfjklsadfjl', 'kediri', 'jawa tengah', '2039489', 'reseller', '1554003122-kaos5.jpg', 0),
+	(3, 'bayucoba', '$2y$10$bKLhgxUoZMRdMR/KYrDyk.L/C90hWCWKX0FC.JilqE82VVvNUr9lC', 'bayukhoirul1@gmail.com', '085123456789', 'bayucoba', 'abcdefghij', 'tulung agung', 'jawa Timur', '0852741', 'biasa', NULL, 0);
+/*!40000 ALTER TABLE `tb_users` ENABLE KEYS */;
+
+-- Dumping structure for table gmk_new.tb_varian
+DROP TABLE IF EXISTS `tb_varian`;
+CREATE TABLE IF NOT EXISTS `tb_varian` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `kode_v` varchar(50) DEFAULT '0',
+  `varian` varchar(50) DEFAULT '0',
+  `hex` varchar(50) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- Dumping data for table gmk_new.tb_varian: 0 rows
+DELETE FROM `tb_varian`;
+/*!40000 ALTER TABLE `tb_varian` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_varian` ENABLE KEYS */;
+
+-- Dumping structure for trigger gmk_new.add_stok
+DROP TRIGGER IF EXISTS `add_stok`;
+SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO';
+DELIMITER //
+CREATE TRIGGER `add_stok` AFTER INSERT ON `tb_tambahstoks` FOR EACH ROW update tb_barangs set stok=stok+new.jumlah where idbarang=new.idwarna//
+DELIMITER ;
+SET SQL_MODE=@OLDTMP_SQL_MODE;
+
+-- Dumping structure for trigger gmk_new.in_stok
+DROP TRIGGER IF EXISTS `in_stok`;
+SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO';
+DELIMITER //
+CREATE TRIGGER `in_stok` AFTER INSERT ON `detail_cancel` FOR EACH ROW update tb_barangs set stok=stok+new.jumlah where idbarang=new.idwarna//
+DELIMITER ;
+SET SQL_MODE=@OLDTMP_SQL_MODE;
+
+-- Dumping structure for trigger gmk_new.keranjang_dihapus
+DROP TRIGGER IF EXISTS `keranjang_dihapus`;
+SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO';
+DELIMITER //
+CREATE TRIGGER `keranjang_dihapus` AFTER INSERT ON `keranjang_cancel` FOR EACH ROW BEGIN
+update tb_barangs set stok=stok+new.jumlah where idbarang=new.idbarang;
+END//
+DELIMITER ;
+SET SQL_MODE=@OLDTMP_SQL_MODE;
+
+-- Dumping structure for trigger gmk_new.min_stok
+DROP TRIGGER IF EXISTS `min_stok`;
+SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO';
+DELIMITER //
+CREATE TRIGGER `min_stok` AFTER INSERT ON `tb_details` FOR EACH ROW update tb_barangs set stok=stok-new.jumlah where idbarang=new.idwarna//
+DELIMITER ;
+SET SQL_MODE=@OLDTMP_SQL_MODE;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
